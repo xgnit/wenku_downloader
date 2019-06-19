4f0228f79c55a7bbb698b3cd40935adc9d8080ad
TF <random@random.com>
2019-06-19 16:20:26 +0200

    finished GUI

--- wenku.py
+++ wenku.py
@@ -1,5 +1,6 @@
 
 import tkinter as tk
+from backend import fetch
 
 window = tk.Tk()
 
@@ -11,12 +12,20 @@ l = tk.Label(window, text='在下方输入文库链接', font=('Arial', 12))
 l.grid(row=0, pady=20)
 
 
-tk.Entry(window,text = '', width=60).grid(row=1)
+link = tk.StringVar()
+tk.Entry(window, textvariable=link, width=60).grid(row=1, columnspan=5)
+
+choices = { 'word','PDF','PPT','txt'}
+choose_var = tk.StringVar()
+choose_var.set('word') # set the default option
+
+tk.Label(window, text='选择文件类型', font=('Arial', 12)).grid(row=2, column=0, pady=20)
+popupMenu = tk.OptionMenu(window, choose_var, *choices).grid(row=2, column=1, pady=20)
 
 def download():
-    print('hello')
+    fetch(link.get(), choose_var.get())
 
 
-tk.Button(window, text='下载', command=download).grid(row=2, pady=20)
+tk.Button(window, text='下载', command=download).grid(row=3, pady=20)
 
 window.mainloop()
